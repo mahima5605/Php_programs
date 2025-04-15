fd0f9ebc2c9504dcc444a9c2b6ff1352b3a91712
mahima5605 <mahimegha5605@gmail.com>
2025-04-15 19:53:39 +0530

    Add files via upload

---
+++ 7.Contact.php
@@ -0,0 +1,7 @@
+<?php
+echo "Customer name is: ".$_POST['fname']."<br>";
+echo "Customer's Email id is: ".$_POST['email']."<br>";
+echo "Customer's Mobile number is: ".$_POST['mobile']."<br>";
+echo "Message given by Customer is : ".$_POST['message']."<br><br>";
+echo "Your details are submitted!";
+?>
---
+++ 7.Restaurant.php
@@ -0,0 +1,9 @@
+<?php
+echo "Customer name is: ".$_POST['fname']."<br>";
+echo "Customer's Email id is: ".$_POST['email']."<br>";
+echo "Customer's Mobile number is: ".$_POST['mobile']."<br>";
+echo "Items selected by Customer are : ".$_POST['item']."<br>";
+echo "No. of items Customer has selected : ".$_POST['quantity']."<br>";
+echo "Customer's Address is: ".$_POST['address']."<br><br>";
+echo "Congratulations! Your order is confirmed";
+?>
